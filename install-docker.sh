df42eb5a1f13ab3fbec06fc3b78dac09dce766de
Mohamed ElKalioby <mkalioby@mkalioby.com>
2022-01-07 21:12:29 +0300

    Add install-docker script

---
+++ install-docker.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+
+apt-get remove docker docker-engine docker.io containerd runc
+apt-get install ca-certificates curl gnupg lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+sudo docker run hello-world
+
