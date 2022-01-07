e811581ea6d7ff2214f50758d208554758cba1de
Mohamed ElKalioby <mkalioby@mkalioby.com>
2022-01-07 23:55:39 +0300

    Fixes to serve static files with whitenoise

--- django_app/k8s/settings.py
+++ django_app/k8s/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!46xoe3^6$08a_rl1+jx9w+wfp=69k2k3cn9ai1iegu5xqrib('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
