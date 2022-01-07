821ff0ca05a13f6765379413eff7f33022c1802d
Mohamed ElKalioby <mkalioby@mkalioby.com>
2022-01-07 19:51:03 +0300

    Inital Working Version

---
+++ README.md
@@ -0,0 +1,7 @@
+# Django on Kubernetes
+
+That is the project we will deploy on Kubernates with Docker as a container engine.
+
+The articles can be found at
+
+Login is admin,admin 
---
+++ k8s/views.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+@login_required()
+def home(request):
+    return render(request,"home.html",{})
+
+
