8063794344417923a445d643a71b778ef66fb973
Mohamed ElKalioby <mkalioby@mkalioby.com>
2022-01-11 14:37:16 +0300

    Running with MySQL

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.8-slim-bullseye
 
+RUN apt update && apt install -y gcc libmariadb-dev-compat
+
 RUN pip install gunicorn
 
 WORKDIR /app
@@ -9,6 +11,9 @@ COPY django_app/requirements.txt .
 RUN pip install -r /app/requirements.txt
 
 COPY django_app/ /app/
+
+CMD sleep 5
+CMD python manage.py migrate
 
 EXPOSE 80/tcp
 
--- django_app/k8s/settings.py
+++ django_app/k8s/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'k8s.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_example',
+        'USER': 'root',
+        'PASSWORD': 'pass1234',
+        'HOST': '127.0.0.1'
     }
 }
 
